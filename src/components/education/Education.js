c17e5368c308091789a9f18b7641a8b34d792d9e
Suraj Patil <suraj.gd89@gmail.com>
2022-03-16 16:45:44 +0530

    new changes16032022

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -1,55 +1,30 @@
 import './education.css';
+import React, { useContext } from 'react';
+import { AppData } from '../../App';
 function Education(props) {
-
+    const { UserData } = useContext(AppData);
+    const education = UserData.education;
 
     return (
         <section className='education'>
             <h2 className='heading'>Education</h2>
             <div className='list'>
-                <div className='item'>
-                    <div className='col-one'>
-                        <h3>Degree</h3>
-                        <div>2019</div>
-                    </div>
-                    <div className='col-two'>
-                        <h3>Bachlor of Arts</h3>
-                        <div> Yashwantrao Chavan Maharashtra Open University</div>
-                    </div>
-                </div>
-
-                <div className='item'>
-                    <div className='col-one'>
-                        <h3>Diploma</h3>
-                        <div>2012</div>
-                    </div>
-                    <div className='col-two'>
-                        <h3>Web &amp; Graphic Designing</h3>
-                        <div>NIDT Multimedia Animation Institute, Chembur</div>
-                    </div>
-                </div>
-
-                <div className='item'>
-                    <div className='col-one'>
-                        <h3>College</h3>
-                        <div>2007</div>
-                    </div>
-                    <div className='col-two'>
-                        <h3>H.S.C</h3>
-                        <div>K. C. College, Talmavale</div>
-                    </div>
-                </div>
-
-                <div className='item'>
-                    <div className='col-one'>
-                        <h3>School</h3>
-                        <div>2005</div>
-                    </div>
-                    <div className='col-two'>
-                        <h3>S.S.C</h3>
-                        <div>S. V. M. High School, Talmavale</div>
-                    </div>
-                </div>
-
+                {
+                    education.map((item, index) => {
+                        return (
+                            <div className='item' key={index}>
+                                <div className='col-one'>
+                                    <h3>{item.level}</h3>
+                                    <div>{item.year}</div>
+                                </div>
+                                <div className='col-two'>
+                                    <h3>{item.name}</h3>
+                                    <div>{item.from}</div>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
             </div>
         </section>
     );
--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,29 +1,71 @@
 import './experience.css';
+import React, { useContext } from 'react';
+import { AppData } from '../../App';
 function Experience() {
+    const { UserData } = useContext(AppData);
+    const experience = UserData.experience;
+    function countExp(fromDate, toDate) {
+        let exp;
+        let current = new Date();
+        let from = new Date(fromDate);
+        let to = new Date(toDate);
+        const monthInmiliseconds = 2590000000;
+
+        if (toDate == "Present") {
+            let difference = new Date(current - from).getTime();
+            let months = Math.floor(difference / monthInmiliseconds);
+            let yr = months / 12;
+            let num = Math.round(yr * 10) / 10;
+            let in_years = num.toString().split('.')[0];
+            let in_months = num.toString().split('.')[1];
+
+            if (in_years === '0') {
+                exp = `${in_years} years`
+            } else if (in_months === '0') {
+                exp = `${in_months} months`
+            } else {
+                exp = `${in_years} year, ${in_months} months`
+            }
+
+        } else {
+            let difference = new Date(to - from).getTime();
+            let months = Math.floor(difference / monthInmiliseconds);
+            let yr = months / 12;
+            let num = Math.round(yr * 10) / 10;
+            let in_years = num.toString().split('.')[0];
+            let in_months = num.toString().split('.')[1];
+            if (in_years === '0') {
+                exp = `${in_years} years`
+            } else if (in_months === '0') {
+                exp = `${in_months} months`
+            } else {
+                exp = `${in_years} year, ${in_months} months`
+            }
+        }
+        return exp;
+    }
+
     return (
         <section className='experience'>
             <h2 className='heading'>Experience</h2>
             <div className='list'>
-                <div className='item'>
-                    <div className='col-one'>
-                        <h3>2017 - Present</h3>
-                        <div> &gt; 4 Years</div>
-                    </div>
-                    <div className='col-two'>
-                        <h3>Infibeam Avenues Ltd.</h3>
-                        <div>Sr. UI Developer</div>
-                    </div>
-                </div>
-                <div className='item'>
-                    <div className='col-one'>
-                        <h3>2013 - 2017</h3>
-                        <div>3.5 Years</div>
-                    </div>
-                    <div className='col-two'>
-                        <h3>Osource Global Ltd.</h3>
-                        <div>Creative Designer</div>
-                    </div>
-                </div>
+                {
+                    experience.map((item, index) => {
+                        return (
+                            <div className='item' key={index}>
+                                <div className='col-one'>
+                                    <h3>{new Date(item.fromDate).getFullYear()} - {item.toDate != "Present" ? new Date(item.toDate).getFullYear() : "Present"}</h3>
+                                    <div>{countExp(item.fromDate, item.toDate)}</div>
+                                </div>
+                                <div className='col-two'>
+                                    <h3>{item.companyName}</h3>
+                                    <div>{item.designation}</div>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+
             </div>
         </section>
     );
