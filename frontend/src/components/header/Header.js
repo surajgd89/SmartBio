90065301200fd5033aa7d37bb4ed88b047863611
Suraj Patil <suraj.gd89@gmail.com>
2024-03-27 14:58:03 +0530

    tue update6

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -3,28 +3,37 @@ import ProfilePicture from '../../components/profile-picture/ProfilePicture';
 import Intro from '../../components/intro/Intro';
 
 function Header({ download, XL, SM, HeaderFixed, sidebarToggle }) {
-
-
     return (
-        <header className={HeaderFixed ? 'header fixed' : 'header'} >
-            <div data-html2canvas-ignore="true" className='toggle' onClick={sidebarToggle}><i className='fal fa-bars'></i></div>
+        <header className={HeaderFixed ? 'header fixed' : 'header'}>
+            <div
+                data-html2canvas-ignore="true"
+                className="toggle"
+                onClick={sidebarToggle}
+            >
+                <i className="fal fa-bars"></i>
+            </div>
             <ProfilePicture />
             {!XL && <Intro />}
             {!SM && <Intro />}
-            <div className='action' >
+            <div className="action">
                 <a href="mailto:suraj.gd89@gmail.com">
-                    <i className='fal fa-envelope'></i>
+                    <i className="fal fa-envelope"></i>
                     <span>Email</span>
                 </a>
                 <a href="tel:919594415153">
-                    <i className='fal fa-phone-alt'></i>
+                    <i className="fal fa-phone-alt"></i>
                     <span>Call</span>
                 </a>
-                {!HeaderFixed ? <button type='button' onClick={download} data-html2canvas-ignore="true">
-                    <i className='fal fa-arrow-to-bottom'></i>
-                    <span>PDF</span>
-                </button> : null}
-
+                {!HeaderFixed ? (
+                    <button
+                        type="button"
+                        onClick={download}
+                        data-html2canvas-ignore="true"
+                    >
+                        <i className="fal fa-arrow-to-bottom"></i>
+                        <span>PDF</span>
+                    </button>
+                ) : null}
             </div>
         </header>
     );
